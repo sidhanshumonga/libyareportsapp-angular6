3e3b54986a78456b428e7ecae8830f1ece57bab3
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-02 16:17:23 +0530

    added new constants required, changed to objects.

--- src/app/CONSTANTS.ts
+++ src/app/CONSTANTS.ts
@@ -1,28 +1,30 @@
 
 export const BASE_URL = "../../";
-export const months = [ 
-    "January", 
-    "February", 
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "september",
-    "October",
-    "November",
-    "December"];
+export const DATASETS_API = "dataSets.json?fields=name,id,attributeValues[value,attribute[id,name]]&paging=none";
+
+export const months = [
+    { name: "January", value: "01" },
+    { name: "February", value: "02" },
+    { name: "March", value: "03" },
+    { name: "April", value: "04" },
+    { name: "May", value: "05" },
+    { name: "June", value: "06" },
+    { name: "July", value: "07" },
+    { name: "August", value: "08" },
+    { name: "september", value: "09" },
+    { name: "October", value: "10" },
+    { name: "November", value: "11" },
+    { name: "December", value: "12" }];
 
 export const quarters = [
-    "January-March",
-    "April-June",
-    "July-September",
-    "October-December"];
+    { name: "January-March", value: "Q1" },
+    { name: "April-June", value: "Q2" },
+    { name: "July-September", value: "Q3" },
+    { name: "October-December", value: "Q4" }];
 
 export const sixmonths = [
-    "January-June",
-    "July-December"
+    { name: "January-June", value: "S1" },
+    { name: "July-December", value: "S2" },
 ];
 
 export const years = [
@@ -39,15 +41,45 @@ export const periods = [
     "Yearly"
 ];
 
-export const DATASET_ID_EWARN_REPORT = "h1Q03rJqNQr";
+export const DATASET_ID_EWARN_REPORT = { id: "h1Q03rJqNQr", name: "EWARN Reporting Dataset" };
 
 //phc datasets uid
-export const DATASETS_ID_PHC = ['sl7pTQ0lY4K','yY9r5kbus3t','DVEYU2A2aF2','WRzYEN981Nb',
-'dL7jX6E0UOB','NL5AxxiJt31','jqSaKxtj8IA','i5doaVaayYN','YcywkvYogw3','qq8aqH1YPjT',
-'Ri1mp3YgF3s','n71AgyCV7gp'];
+export const DATASETS_ID_PHC = [
+    { id: 'sl7pTQ0lY4K', name: "Population Data Set" },
+    { id: 'yY9r5kbus3t', name: "PHC OPD Dataset" },
+    { id: 'DVEYU2A2aF2', name: "PHC Emergency Care Dataset" },
+    { id: 'WRzYEN981Nb', name: "Dental Services Dataset" },
+    { id: 'dL7jX6E0UOB', name: "Diabetes Services Dataset" },
+    { id: 'NL5AxxiJt31', name: "MCH and Family Planning Dataset" },
+    { id: 'jqSaKxtj8IA', name: "Immunisation Dataset" },
+    { id: 'i5doaVaayYN', name: "PHC Radiology Services Dataset" },
+    { id: 'YcywkvYogw3', name: "PHC Laboratory Services Dataset" },
+    { id: 'qq8aqH1YPjT', name: "Essential Drugs and Consumables Stocks Dataset" },
+    { id: 'Ri1mp3YgF3s', name: "PHC Human Resource Dataset" },
+    { id: 'n71AgyCV7gp', name: "Physiotherapy Services Dataset" }];
 
-export const DATASETS_ID_HOSPITAL = ['tgGyJAaIahr','bGnNmKSuZlw','vQeZkk8Fz2y','CFDWUtgFaSX',
-'ShLvZi45WYv','FN9wO5d2jmN','TKR665WfTQd','NVirEUvg4lE','fyIza2Ggy2G','n71AgyCV7gp'];
+export const DATASETS_ID_HOSPITAL = [
+    { id: 'IgbqlaxE83v', name: "Hospital OPD Services Dataset" },
+    { id: 'tgGyJAaIahr', name: "Hospital/Medical Centre Emergency Care Dataset" },
+    { id: 'bGnNmKSuZlw', name: "Hospital IPD Services Dataset" },
+    { id: 'vQeZkk8Fz2y', name: "Hospital/Medical Centre Delivery and Birth Services Dataset" },
+    { id: 'CFDWUtgFaSX', name: "Hospital Surgery Services Dataset" },
+    { id: 'ShLvZi45WYv', name: "Hospital/Medical Centre Radiology Services Dataset" },
+    { id: 'FN9wO5d2jmN', name: "Hospital/Medical Centre Laboratory and Blood Bank Services Dataset" },
+    { id: 'TKR665WfTQd', name: "Hospital Human Resource Dataset" },
+    { id: 'NVirEUvg4lE', name: "Hospital/Medical Centre Infectious Disease and Deaths Dataset" },
+    { id: 'fyIza2Ggy2G', name: "Hospital/Medical Centre Infectious Disease Dataset (Age group)" },
+    { id: 'n71AgyCV7gp', name: "Physiotherapy Services Dataset" }];
 
-export const DATASETS_ID_MEDICALCENTER = ['KeIRqetnAE2','tgGyJAaIahr','hOAXV4EYKNG','vQeZkk8Fz2y',
-'EUzM2mlJQm4','ShLvZi45WYv','FN9wO5d2jmN','w9qPPMwmPJy','NVirEUvg4lE','fyIza2Ggy2G','n71AgyCV7gp'];
+export const DATASETS_ID_MEDICALCENTER = [
+    { id: 'KeIRqetnAE2', name: "Medical Centre OPD Services Dataset" },
+    { id: 'tgGyJAaIahr', name: "Hospital/Medical Centre Emergency Care Dataset" },
+    { id: 'hOAXV4EYKNG', name: "Medical Centre IPD Services Dataset" },
+    { id: 'vQeZkk8Fz2y', name: "Hospital/Medical Centre Delivery and Birth Services Dataset" },
+    { id: 'EUzM2mlJQm4', name: "Medical Centre Surgery Services Dataset" },
+    { id: 'ShLvZi45WYv', name: "Hospital/Medical Centre Radiology Services Dataset" },
+    { id: 'FN9wO5d2jmN', name: "Hospital/Medical Centre Laboratory and Blood Bank Services Dataset" },
+    { id: 'w9qPPMwmPJy', name: "Medical Centre Human Resource Dataset" },
+    { id: 'NVirEUvg4lE', name: "Hospital/Medical Centre Infectious Disease and Deaths Dataset" },
+    { id: 'fyIza2Ggy2G', name: "Hospital/Medical Centre Infectious Disease Dataset (Age group)" },
+    { id: 'n71AgyCV7gp', name: "Physiotherapy Services Dataset" }];
