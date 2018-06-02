509576b531a13d18832b72c79ca523fc24c806e3
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-02 05:42:39 +0530

    Adding constants: baseURL and Datasets id

--- src/app/CONSTANTS.ts
+++ src/app/CONSTANTS.ts
@@ -1,3 +1,5 @@
+
+export const BASE_URL = "../../";
 export const months = [ 
     "January", 
     "February", 
@@ -36,3 +38,16 @@ export const periods = [
     "Six-monthly",
     "Yearly"
 ];
+
+export const DATASET_ID_EWARN_REPORT = "h1Q03rJqNQr";
+
+//phc datasets uid
+export const DATASETS_ID_PHC = ['sl7pTQ0lY4K','yY9r5kbus3t','DVEYU2A2aF2','WRzYEN981Nb',
+'dL7jX6E0UOB','NL5AxxiJt31','jqSaKxtj8IA','i5doaVaayYN','YcywkvYogw3','qq8aqH1YPjT',
+'Ri1mp3YgF3s','n71AgyCV7gp'];
+
+export const DATASETS_ID_HOSPITAL = ['tgGyJAaIahr','bGnNmKSuZlw','vQeZkk8Fz2y','CFDWUtgFaSX',
+'ShLvZi45WYv','FN9wO5d2jmN','TKR665WfTQd','NVirEUvg4lE','fyIza2Ggy2G','n71AgyCV7gp'];
+
+export const DATASETS_ID_MEDICALCENTER = ['KeIRqetnAE2','tgGyJAaIahr','hOAXV4EYKNG','vQeZkk8Fz2y',
+'EUzM2mlJQm4','ShLvZi45WYv','FN9wO5d2jmN','w9qPPMwmPJy','NVirEUvg4lE','fyIza2Ggy2G','n71AgyCV7gp'];
