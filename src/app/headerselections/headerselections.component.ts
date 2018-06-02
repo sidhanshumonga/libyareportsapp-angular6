99129fff6a8b3d71343154e8a2cd4c65ca554dc0
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-02 16:19:29 +0530

    id of dataset included in response. model value changed to object

--- src/app/headerselections/headerselections.component.ts
+++ src/app/headerselections/headerselections.component.ts
@@ -23,30 +23,12 @@ export class HeaderselectionsComponent {
   ];
   //multiple selection form control for datasets
   multidatasets = new FormControl();
-  selectedvalues:any [];
+  selectedvalues:{
+    id:string,
+    name:string
+  };
   reportName:string;
-  datasetsArray = [
-    // { value: 'dataset1 dataset1', viewValue: 'dataset1' },
-    // { value: 'dataset2 dataset1dataset1', viewValue: 'dataset2' },
-    // { value: 'dataset3 dataset1dataset1', viewValue: 'dataset3' },
-    // { value: 'dataset4 dataset1 dataset1 dataset1', viewValue: 'dataset4' },
-    // { value: 'dataset5', viewValue: 'dataset5' },
-    // { value: 'dataset6', viewValue: 'dataset6' },
-    // { value: 'dataset7', viewValue: 'dataset7' },
-    // { value: 'dataset8', viewValue: 'dataset8' },
-    // { value: 'dataset9', viewValue: 'dataset9' },
-    // { value: 'dataset10', viewValue: 'dataset10' },
-    // { value: 'dataset11', viewValue: 'dataset10' },
-    // { value: 'dataset12', viewValue: 'dataset10' },
-    // { value: 'dataset13', viewValue: 'dataset10' },
-    // { value: 'dataset14', viewValue: 'dataset10' },
-    // { value: 'dataset15', viewValue: 'dataset10' },
-    // { value: 'dataset16', viewValue: 'dataset10' },
-    // { value: 'dataset17', viewValue: 'dataset10' },
-    // { value: 'dataset18', viewValue: 'dataset10' },
-    // { value: 'dataset19', viewValue: 'dataset10' },
-    // { value: 'dataset20', viewValue: 'dataset10' },
-  ];
+  datasetsArray = [];
 
   constructor(private chipsService: SharedService, private ajaxService: AjaxserviceService) {
     //method which gets values from datasettabs!
@@ -73,10 +55,8 @@ export class HeaderselectionsComponent {
                   if(attr[j].attribute.name == 'Report app' && attr[j].value == 'true'){
                       for(var k = 0; k < attr.length; k++){
                           if(attr[k].attribute.name != 'Report app' && attr[k].value == 'true'){
-                            // if(attr[k].attribute.name == $scope.reportButton){
-                                  var obj = {'viewValue': datasets[i].name, 'value':attr[k].attribute.name,}; // attr[k].attribute.name};
+                                  var obj = {'viewValue': datasets[i].name, 'value':attr[k].attribute.name, 'id':datasets[i].id}; // attr[k].attribute.name};
                                   this.datasetsArray.push(obj);
-                            // }
                           }
                       }
                   }
