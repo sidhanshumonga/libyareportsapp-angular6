3a108ef97c8761532e15c734fa8f21f61c6e0a49
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-02 05:44:42 +0530

    comments added

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,33 +1,40 @@
 import { Injectable } from '@angular/core';
-import { Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HeaderselectionsComponent } from 'src/app/headerselections/headerselections.component';
 import { DatasetstabsComponent } from 'src/app/datasetstabs/datasetstabs.component';
-import { Subject } from  'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-  
+
   constructor() {
   }
-  
+
   chips: string[];
 
-  // Observable string sources
+  //code to call function from header to datasettabs
   private methodToChangeChips = new Subject<any>();
-  private methodToUnselect = new Subject<any>();
-
-   // Observable string streams
   chipServiceMethod = this.methodToChangeChips.asObservable();
-  unselectServiceMethod = this.methodToUnselect.asObservable();
-
-  //function to be called by component after further changes!
   callMethodToChangeChips(chipss) {
     this.methodToChangeChips.next(chipss);
   }
+
+
+  //code to call function from datasettabs to header
+  private methodToUnselect = new Subject<any>();
+  unselectServiceMethod = this.methodToUnselect.asObservable();
   callMethodToUnselect(chipss) {
     this.methodToUnselect.next(chipss);
   }
-  
+
+  //code to call function from header to datasettabs
+  private methodTovalidatePeriods = new Subject<any>();
+  periodValidateServiceMedthod = this.methodTovalidatePeriods.asObservable();
+  callMethodToValidatePeriods(value){
+    this.methodTovalidatePeriods.next(value);
+  }
+
+
 }
