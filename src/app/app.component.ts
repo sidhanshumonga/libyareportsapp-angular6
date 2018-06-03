5307d52a585b3e639bce97f760af746116e8fc83
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-04 04:32:21 +0530

    added loader functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import {Component,ViewEncapsulation} from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { DatasetstabsComponent } from 'src/app/datasetstabs/datasetstabs.component';
 import { HeaderselectionsComponent } from 'src/app/headerselections/headerselections.component';
 import { RightbarselectionsComponent } from 'src/app/rightbarselections/rightbarselections.component';
 import { OrgunitlibraryComponent } from 'src/app/orgunitlibrary/orgunitlibrary.component';
+import { LoaderComponent } from 'src/app/loader/loader.component'
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-component',
   styleUrls: ['app.component.css'],
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    setTimeout(() => {
+      $("#loader").fadeOut(500);
+      $("#mainPage").fadeIn(800);
+    }, 3000);
+  }
+
 }
 
