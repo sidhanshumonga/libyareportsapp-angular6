77a76b81eac29cd9023d8afa78eabeeae216d723
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-04 14:42:57 +0530

    function added to receive array of values from mainselection. sending values to ajax function and getting html response

--- src/app/tablecard/tablecard.component.ts
+++ src/app/tablecard/tablecard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+import { AjaxserviceService } from 'src/app/ajaxservice.service';
 
 @Component({
   selector: 'app-tablecard',
@@ -7,27 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TablecardComponent {
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-}
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+  ou:string;
+  pe:string;
+  ds:string;
+
+  constructor(private callingBridge: SharedService, private ajax: AjaxserviceService) {
+    //method service which gets selectedOrgUnit from orgunitlibrary
+    this.callingBridge.paramsServiceMethod.subscribe(
+      (params) => {
+        this.ou = params[0];
+        this.pe = params[1];
+        this.ds = params[2];
+        this.displayReport();
+      }
+    );
+  }
+
+  displayReport(){
+    this.ajax.getDatasetHTML(this.ou,this.pe,this.ds).subscribe(res => {
+      console.log(res);
+      setTimeout(() => {
+        this.modifyReport(res);
+      }, 1000);
+      
+    });
+  }
+
+  modifyReport(response){
+
+  }
+
+}
 
