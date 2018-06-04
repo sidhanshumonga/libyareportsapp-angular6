6e591409ef4855f29772294053333744ce346be9
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-04 15:49:53 +0530

    code to modify html table added

--- src/app/tablecard/tablecard.component.ts
+++ src/app/tablecard/tablecard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/shared.service';
 import { AjaxserviceService } from 'src/app/ajaxservice.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-tablecard',
@@ -11,9 +12,9 @@ import { AjaxserviceService } from 'src/app/ajaxservice.service';
 export class TablecardComponent {
 
 
-  ou:string;
-  pe:string;
-  ds:string;
+  ou: string;
+  pe: string;
+  ds: string;
 
   constructor(private callingBridge: SharedService, private ajax: AjaxserviceService) {
     //method service which gets selectedOrgUnit from orgunitlibrary
@@ -27,19 +28,26 @@ export class TablecardComponent {
     );
   }
 
-  displayReport(){
-    this.ajax.getDatasetHTML(this.ou,this.pe,this.ds).subscribe(res => {
-      console.log(res);
-      setTimeout(() => {
+  displayReport() {
+    this.ajax.getDatasetHTML(this.ou, this.pe, this.ds).subscribe(res => {
         this.modifyReport(res);
-      }, 1000);
-      
     });
   }
 
-  modifyReport(response){
-
+  modifyReport(response) {
+    $("#custom-table").empty();
+    $("#custom-table").append(response);
+    $("#custom-table table").removeAttr("style");
+    $("#custom-table table tr td span span").removeAttr("style");
+    $("#custom-table table tr td span").removeAttr("style");
+    $("#custom-table table tr td").removeAttr("style");
+    $("#custom-table table tr").removeAttr("style");
+    $("#custom-table table tr").removeAttr("height");
+    $("#custom-table table tr td").removeAttr("bgcolor");
+    $("#custom-table table tr td").removeAttr("height");
+    $("#custom-table table tr td").removeAttr("width");
+    $("#custom-table table").addClass("table table-bordered table-hover");
+    $("#custom-table table").attr("width","100%");
   }
-
 }
 
