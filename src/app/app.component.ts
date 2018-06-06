87d60b14f7a4257c0f797553115be582f8dbbb11
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-06-06 06:10:17 +0530

    Clicking on button function added to display sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, ElementRef, AfterViewInit  } from '@angular/core';
 import { DatasetstabsComponent } from 'src/app/datasetstabs/datasetstabs.component';
 import { HeaderselectionsComponent } from 'src/app/headerselections/headerselections.component';
 import { RightbarselectionsComponent } from 'src/app/rightbarselections/rightbarselections.component';
@@ -12,6 +12,21 @@ import * as $ from 'jquery';
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
+ constructor(private onclicks: ElementRef,){
+
+ }
+
+ ngAfterViewInit(){
+  var classes = this.onclicks.nativeElement.querySelector('#sidebarCollapse');
+  classes.addEventListener('click', this.hideme.bind(this));
+ }
+hideme(){
+  // open sidebar
+  $('#sidebar').addClass('active');
+  // fade in the overlay
+  $('.overlay').fadeIn();
+}
+
   ngOnInit() {
     setTimeout(() => {
       $("#loader").fadeOut(500);
